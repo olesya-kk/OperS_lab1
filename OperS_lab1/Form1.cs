0a1979a45054902bd71b25f894497589d577b346
Admin <Admin@LAPTOP-Q6I9MM59>
2024-02-11 19:25:15 +0300

    Добавьте файлы проекта.

---
+++ OperS_lab1/Form1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace OperS_lab1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnClickThis_Click(object sender, EventArgs e)
+        {
+            if (int.TryParse(textBox1.Text, out int number))//проверка, является ли введеное значение числом
+            {
+                int result = number * number - 3 + number;
+                textBox1.Text = result.ToString();
+            }
+            else 
+            {
+                MessageBox.Show("Неправильный ввод!");
+            }
+        }
+    }
+}
